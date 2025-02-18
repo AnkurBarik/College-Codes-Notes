8da68c977b20ebe0a1f327be928f4b31f4e0bd94
Ankur Barik <154061998+AnkurBarik@users.noreply.github.com>
2025-02-18 16:38:57 +0530

    .

---
+++ Semester 4/Full Stack Development--1/Assignment3.js
@@ -0,0 +1,23 @@
+const args = process.argv.slice(2)
+const eft=(f)=>{
+    console.time()
+    f()
+    console.timeEnd()
+}
+const add=(a,b)=>{
+    return a+b
+}
+const sub=(a,b)=>{
+    return a-b
+}
+const mul=(a,b)=>{
+    return a*b
+}
+const div=(a,b)=>{
+    return a/b
+}
+
+eft(()=>console.log(add(5, 10)))
+eft(()=>console.log(sub(15, 10)))
+eft(()=>console.log(mul(5, 10)))
+eft(()=>console.log(div(15, 3)))
