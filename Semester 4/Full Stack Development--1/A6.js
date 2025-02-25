4bd083a2a669c19f2224e70cd1cecb74b0b89522
Ankur Barik <154061998+AnkurBarik@users.noreply.github.com>
2025-02-25 17:34:44 +0530

    fghn

---
+++ Semester 4/Full Stack Development--1/A6.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+function rFSE() {
+  try {
+    const data = fs.readFileSync('example.txt', 'utf8');
+    console.log('Synchronous File Data:', data);
+  } catch (err) {
+    console.error('Error reading file synchronously:', err);
+  }
+  console.log('Synchronous File Read End');
+}
+function rFAE() {
+  fs.readFile('example.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file asynchronously:', err);
+    } else {
+      console.log('Asynchronous File Data:', data);
+    }
+    console.log('Asynchronous File Read End');
+  });
+}
+
+rFSE();
+rFAE();
+
+console.log('Code Execution Completed');
--- Semester 4/Full Stack Development--1/WriteFileSync.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = 'Hello World.';
-fs.writeFile('main.c', content, (err) => {
-  if (err) {
-    console.error('Error occured', err);
-  } else {
-    console.log('File has been created');
-  }
-});
