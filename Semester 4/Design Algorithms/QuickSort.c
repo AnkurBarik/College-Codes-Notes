68cac064e2fb7f3a6ae37f9f763bcaaa3446e1af
Ankur Barik <154061998+AnkurBarik@users.noreply.github.com>
2025-03-08 13:12:43 +0530

    12333331q

--- Semester 4/Design Algorithms/QuickSort.c
+++ Semester 4/Design Algorithms/QuickSort.c
@@ -7,32 +7,24 @@ void swap(int* a, int* b) {
     *b = temp;
 }
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
-    
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-void quickSort(int arr[], int low, int high) {
+int QuickSort(int arr[], int low, int high) {
     if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        int pivot = arr[high];
+        int i = (low - 1);
+        
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(&arr[i], &arr[j]);
+            }
+        }
+        swap(&arr[i + 1], &arr[high]);
+        int pi = i + 1;
+      
+        QuickSort(arr, low, pi - 1);
+        QuickSort(arr, pi + 1, high);
     }
-}
-
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+    return 0;
 }
 
 int main() {
@@ -41,18 +33,22 @@ int main() {
     scanf("%d", &n);
     
     int arr[n];
-    printf("Enter %d integers: ", n);
+    printf("Enter %d elements:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
     
     printf("Original array: ");
-    printArray(arr, n);
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
     
-    quickSort(arr, 0, n - 1);
+    QuickSort(arr, 0, n - 1);
     
     printf("Sorted array: ");
-    printArray(arr, n);
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
     
     return 0;
 }
